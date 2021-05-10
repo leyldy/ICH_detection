75203fe1dbe678c22ae705b2dfcac20414ccf47a
jonghalee <jonghalee31@gmail.com>
2021-05-10 18:01:38 +0000

    Trying to make into module

--- model/baseline_3d_cnn.py
+++ model/baseline_3d_cnn.py
@@ -4,10 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class FeatureExtractor(nn.Module):
-    def __init__(self, in_num_ch=1, img_size=(32,64,64), inter_num_ch=16, kernel_size=3, conv_act='relu',dropout=0.2):
-        super(FeatureExtractor, self).__init__()
-
+class baseline_3DCNN(nn.Module):
+    def __init__(self, in_num_ch=1, img_size=(12,512,512), inter_num_ch=16, kernel_size=3, conv_act='relu',dropout=0.2):
+        super(baseline_3DCNN, self).__init__()
 
         self.conv1 = nn.Sequential(
                         nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1),
@@ -34,6 +33,21 @@ class FeatureExtractor(nn.Module):
                         nn.ReLU(inplace=True),
                         nn.BatchNorm3d(2*inter_num_ch),
                         nn.MaxPool3d(2))
+        
+        self.lin1 = nn.Sequential(
+            nn.Dropout(args.dropout),
+            nn.Linear(2048, 128),
+            nn.ReLU()
+        )
+        self.lin2 = nn.Sequential(
+                    nn.Dropout(args.dropout),
+                    nn.Linear(128, 16),
+                    nn.ReLU(),
+        )
+        self.lin3 = nn.Sequential(
+            nn.Dropout(args.dropout),
+            nn.Linear(16, 6),
+        )
 
 
     def forward(self, x):
@@ -42,16 +56,7 @@ class FeatureExtractor(nn.Module):
         conv2 = self.conv2(conv1)
         conv3 = self.conv3(conv2)
         conv4 = self.conv4(conv3)
+        out = self.lin1(conv4)
+        out = self.lin2(out)
+        out = self.lin3(out)
         return conv4
-
-
-classifier = nn.Sequential(
-                    nn.Dropout(args.dropout),
-                    nn.Linear(2048, 128),
-                    nn.ReLU(),
-                    nn.Dropout(args.dropout),
-                    nn.Linear(128, 16),
-                    nn.ReLU(),
-                    nn.Dropout(args.dropout),
-                    nn.Linear(16, 2),
-                ).to(device)
