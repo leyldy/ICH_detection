9fb2314ab645e3f53a49141371c518d87b7c4150
jonghalee <jonghalee31@gmail.com>
2021-05-10 20:30:59 +0000

    Work on model and dataloader

--- model/baseline_3d_cnn.py
+++ model/baseline_3d_cnn.py
@@ -1,12 +1,17 @@
+import sys
+sys.path.append('..')
+
 import torch
 from torch.nn.parameter import Parameter
 import torch.nn as nn
 import torch.nn.functional as F
 
+from utils.model_utils import *
+
 
 class baseline_3DCNN(nn.Module):
-    def __init__(self, in_num_ch=1, img_size=(12,512,512), inter_num_ch=16, kernel_size=3, conv_act='relu',dropout=0.2):
-        super(baseline_3DCNN, self).__init__()
+    def __init__(self, in_num_ch=1, img_size=(12,512,512), inter_num_ch=4, kernel_size=3, conv_act='relu',dropout=0.2):
+        super().__init__()
 
         self.conv1 = nn.Sequential(
                         nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1),
@@ -35,28 +40,34 @@ class baseline_3DCNN(nn.Module):
                         nn.MaxPool3d(2))
         
         self.lin1 = nn.Sequential(
-            nn.Dropout(args.dropout),
-            nn.Linear(2048, 128),
+            nn.Dropout(dropout),
+            nn.Linear(65536, 128),
             nn.ReLU()
         )
         self.lin2 = nn.Sequential(
-                    nn.Dropout(args.dropout),
+                    nn.Dropout(dropout),
                     nn.Linear(128, 16),
                     nn.ReLU(),
         )
         self.lin3 = nn.Sequential(
-            nn.Dropout(args.dropout),
+            nn.Dropout(dropout),
             nn.Linear(16, 6),
         )
 
 
     def forward(self, x):
 
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(conv1)
-        conv3 = self.conv3(conv2)
-        conv4 = self.conv4(conv3)
-        out = self.lin1(conv4)
+        conv_out = self.conv1(x)
+        print(conv_out.shape)
+        conv_out = self.conv2(conv_out)
+        print(conv_out.shape)
+        conv_out = self.conv3(conv_out)
+        print(conv_out.shape)
+#         conv4 = self.conv4(conv3)
+#         print(conv4.shape)
+        out = flatten(conv_out)
+        out = self.lin1(out)
         out = self.lin2(out)
         out = self.lin3(out)
-        return conv4
+        return out
+
