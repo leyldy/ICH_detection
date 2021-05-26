08016a18cc93bf1a8f54290b45822be3ea511054
Jiying Zou <jiyingzou@gmail.com>
2021-05-26 06:26:16 +0000

    baseline model not working

--- model/baseline_3d_cnn.py
+++ model/baseline_3d_cnn.py
@@ -8,40 +8,41 @@ import torch.nn.functional as F
 
 from utils.model_utils import *
 
+# Note: need to recompute dim sizes
 
 class baseline_3DCNN(nn.Module):
-    def __init__(self, in_num_ch=1, img_size=(12,512,512), inter_num_ch=4, kernel_size=3, conv_act='relu',dropout=0.2):
+    def __init__(self, in_num_ch=1, img_size=(40,512,512), inter_num_ch=4, kernel_size=3, conv_act='relu',dropout=0.2):
         super().__init__()
 
         self.conv1 = nn.Sequential(
-                        nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1),
+                        nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1), #(1,40,514,514) --> (4,40,512,512)
                         nn.ReLU(inplace=True),
                         nn.BatchNorm3d(inter_num_ch),
-                        nn.MaxPool3d(2))
+                        nn.MaxPool3d(2)) #(4,40,512,512) --> (4,20,256,256)
 
         self.conv2 = nn.Sequential(
-                        nn.Conv3d(inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
+                        nn.Conv3d(inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1), #(4,20,258,258) --> (8,20,256,256)
                         nn.ReLU(inplace=True),
                         nn.BatchNorm3d(2*inter_num_ch),
-                        nn.MaxPool3d(2),
+                        nn.MaxPool3d(2), #(8,20,256,256) --> (8,10,128,128)
                         nn.Dropout3d(dropout))
 
         self.conv3 = nn.Sequential(
-                        nn.Conv3d(2*inter_num_ch, 4*inter_num_ch, kernel_size=3, padding=1),
+                        nn.Conv3d(2*inter_num_ch, 4*inter_num_ch, kernel_size=3, padding=1), #(8,10,130,130) --> (16,10,128,128)
                         nn.ReLU(inplace=True),
                         nn.BatchNorm3d(4*inter_num_ch),
-                        nn.MaxPool3d(2),
-                        nn.Dropout3d(dropout))
+                        nn.MaxPool3d(2)) #(16,10,128,128) --> (16,5,64,64)
+                        #nn.Dropout3d(dropout))
 
-        self.conv4 = nn.Sequential(
-                        nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
-                        nn.ReLU(inplace=True),
-                        nn.BatchNorm3d(2*inter_num_ch),
-                        nn.MaxPool3d(2))
+#         self.conv4 = nn.Sequential(
+#                         nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
+#                         nn.ReLU(inplace=True),
+#                         nn.BatchNorm3d(2*inter_num_ch),
+#                         nn.MaxPool3d(2))
         
         self.lin1 = nn.Sequential(
             nn.Dropout(dropout),
-            nn.Linear(65536, 128),
+            nn.Linear(16*5*64*64, 128),
             nn.ReLU()
         )
         self.lin2 = nn.Sequential(
@@ -62,7 +63,7 @@ class baseline_3DCNN(nn.Module):
         conv_out = self.conv2(conv_out)
 #         print(conv_out.shape)
         conv_out = self.conv3(conv_out)
-#         print(conv_out.shape)
+        print(conv_out.shape)
 #         conv4 = self.conv4(conv3)
 #         print(conv4.shape)
         out = flatten(conv_out)
