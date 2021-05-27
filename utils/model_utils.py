d29641ba4a402ae044a00147ffbbf603302a2a00
Jiying Zou <jiyingzou@gmail.com>
2021-05-27 03:40:32 +0000

    minor changes

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 
+
 def flatten(x):
     N = x.shape[0] # read in N, C, H, W
     return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
