59937d902c3dd1dd3b9d117aed5929af958ea7eb
leyldy <jonghalee31@gmail.com>
2021-05-28 18:55:12 +0000

    Changed baseline model architecture to fc3 c3

--- model/baseline_3d_cnn.py
+++ model/baseline_3d_cnn.py
@@ -28,12 +28,12 @@ class baseline_3DCNN(nn.Module):
                         nn.MaxPool3d(4), #(8,20,256,256) --> (8,10,128,128)
                         nn.Dropout3d(dropout))
 
-#         self.conv3 = nn.Sequential(
-#                         nn.Conv3d(2*inter_num_ch, 4*inter_num_ch, kernel_size=3, padding=1), #(8,10,130,130) --> (16,10,128,128)
-#                         nn.ReLU(inplace=True),
-#                         nn.BatchNorm3d(4*inter_num_ch),
-#                         nn.MaxPool3d(2)) #(16,10,128,128) --> (16,5,64,64)
-#                         #nn.Dropout3d(dropout))
+        self.conv3 = nn.Sequential(
+                        nn.Conv3d(2*inter_num_ch, 4*inter_num_ch, kernel_size=3, padding=1), #(8,10,130,130) --> (16,10,128,128)
+                        nn.ReLU(inplace=True),
+                        nn.BatchNorm3d(4*inter_num_ch),
+                        nn.MaxPool3d(2)) #(16,10,128,128) --> (16,5,64,64)
+                        #nn.Dropout3d(dropout))
 
 #         self.conv4 = nn.Sequential(
 #                         nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
@@ -43,18 +43,18 @@ class baseline_3DCNN(nn.Module):
         
         self.lin1 = nn.Sequential(
             nn.Dropout(dropout),
-            nn.Linear(8*5*32*32, 128),
+            nn.Linear(16*5*32*32, 128),
             nn.ReLU()
         )
         self.lin2 = nn.Sequential(
                     nn.Dropout(dropout),
-                    nn.Linear(128, 6),
+                    nn.Linear(128, 16),
                     nn.ReLU(),
         )
-#         self.lin3 = nn.Sequential(
-#             nn.Dropout(dropout),
-#             nn.Linear(16, 6),
-#         )
+        self.lin3 = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(16, 6),
+        )
 
 
     def forward(self, x):
@@ -67,13 +67,13 @@ class baseline_3DCNN(nn.Module):
 #         print(conv_out.shape)
         conv_out = self.conv2(conv_out)
 #         print(conv_out.shape)
-#         conv_out = self.conv3(conv_out)
+        conv_out = self.conv3(conv_out)
 #         print(conv_out.shape)
 #         conv4 = self.conv4(conv3)
 #         print(conv4.shape)
         out = flatten(conv_out)
         out = self.lin1(out)
         out = self.lin2(out)
-#         out = self.lin3(out)
+        out = self.lin3(out)
         return out
 
