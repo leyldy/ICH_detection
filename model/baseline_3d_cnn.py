d0e8f5372ed2358b189805ba7a2fabe7340f220c
leyldy <jonghalee31@gmail.com>
2021-05-29 22:24:06 +0000

    make baseline comparable to experiment model

--- model/baseline_3d_cnn.py
+++ model/baseline_3d_cnn.py
@@ -10,44 +10,45 @@ from torchvision import transforms
 from utils.model_utils import *
 
 class baseline_3DCNN(nn.Module):
-    def __init__(self, in_num_ch=1, img_size=(40,256,256), inter_num_ch=4, kernel_size=3, conv_act='relu',dropout=0.2):
+    def __init__(self, in_num_ch=1, img_size=(40, 256, 256), inter_num_ch=4, kernel_size=3, conv_act='relu', dropout=0.2):
         super().__init__()
 
         self.conv1 = nn.Sequential(
-                        nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1), #(1,40,256,256) --> (4,40,256,256)
-                        nn.ReLU(inplace=True),
-                        nn.BatchNorm3d(inter_num_ch),
-                        nn.MaxPool3d(2)) #(4,40,256,256) --> (4,20,128,128)
+            nn.Conv3d(in_num_ch, inter_num_ch, kernel_size=3, padding=1),  # (1,40,256,256) --> (4,40,256,256)
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(inter_num_ch),
+            nn.MaxPool3d(2))  # (4,40,256,256) --> (4,20,128,128)
 
         self.conv2 = nn.Sequential(
-                        nn.Conv3d(inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1), #(4,20,128,128) --> (8,20,128,128)
-                        nn.ReLU(inplace=True),
-                        nn.BatchNorm3d(2*inter_num_ch),
-                        nn.MaxPool3d(2), #(8,20,128,128) --> (8,10,64,64)
-                        nn.Dropout3d(dropout))
+            nn.Conv3d(inter_num_ch, 2 * inter_num_ch, kernel_size=3, padding=1),  # (4,20,128,128) --> (8,20,128,128)
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(2 * inter_num_ch),
+            nn.MaxPool3d(2),  # (8,20,128,128) --> (8,10,64,64)
+            nn.Dropout3d(dropout))
 
         self.conv3 = nn.Sequential(
-                        nn.Conv3d(2*inter_num_ch, 4*inter_num_ch, kernel_size=3, padding=1), #(8,10,64,64) --> (16,10,64,64)
-                        nn.ReLU(inplace=True),
-                        nn.BatchNorm3d(4*inter_num_ch),
-                        nn.MaxPool3d(2)) #(16,10,64,64) --> (16,5,32,32)
-                        #nn.Dropout3d(dropout))
+            nn.Conv3d(2 * inter_num_ch, 1, kernel_size=3, padding=1),  # (8,10,64,64) --> (1,10,64,64)
+            nn.ReLU(inplace=True),
+            nn.BatchNorm3d(1),
+            nn.MaxPool3d(2))  # (1,10,64,64) --> (1,5,32,32)
+#           nn.Dropout3d(dropout))
 
 #         self.conv4 = nn.Sequential(
-#                         nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
-#                         nn.ReLU(inplace=True),
-#                         nn.BatchNorm3d(2*inter_num_ch),
-#                         nn.MaxPool3d(2))
+#                      nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
+#                      nn.ReLU(inplace=True),
+#                      nn.BatchNorm3d(2*inter_num_ch),
+#                      nn.MaxPool3d(2))
         
+        embed_dim = 1*5*32*32
         self.lin1 = nn.Sequential(
             nn.Dropout(dropout),
-            nn.Linear(16*5*32*32, 128),
+            nn.Linear(embed_dim, 64),
             nn.ReLU()
         )
         self.lin2 = nn.Sequential(
-                    nn.Dropout(dropout),
-                    nn.Linear(128, 16),
-                    nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(64, 16),
+            nn.ReLU(),
         )
         self.lin3 = nn.Sequential(
             nn.Dropout(dropout),
@@ -66,9 +67,8 @@ class baseline_3DCNN(nn.Module):
         conv_out = self.conv1(trans_x)
         conv_out = self.conv2(conv_out)
         conv_out = self.conv3(conv_out)
-#         print(conv_out.shape)
-#         conv4 = self.conv4(conv3)
-#         print(conv4.shape)
+        # conv4 = self.conv4(conv3)
+        # print(conv4.shape)
         out = flatten(conv_out)
         out = self.lin1(out)
         out = self.lin2(out)
--- model/selfattn_3d_cnn.py
+++ model/selfattn_3d_cnn.py
@@ -15,8 +15,7 @@ from utils.SelfAttention3D import *
 # Note: need to recompute dim sizes
 
 class selfattn_3DCNN(nn.Module):
-    def __init__(self, in_num_ch=1, img_size=(40, 256, 256), inter_num_ch=4, kernel_size=3, conv_act='relu',
-                 dropout=0.2):
+    def __init__(self, in_num_ch=1, img_size=(40, 256, 256), inter_num_ch=4, kernel_size=3, conv_act='relu', dropout=0.2):
         super().__init__()
 
         self.conv1 = nn.Sequential(
@@ -37,13 +36,13 @@ class selfattn_3DCNN(nn.Module):
             nn.ReLU(inplace=True),
             nn.BatchNorm3d(1),
             nn.MaxPool3d(2))  # (1,10,64,64) --> (1,5,32,32)
-        # nn.Dropout3d(dropout))
+#           nn.Dropout3d(dropout))
 
-        #         self.conv4 = nn.Sequential(
-        #                         nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
-        #                         nn.ReLU(inplace=True),
-        #                         nn.BatchNorm3d(2*inter_num_ch),
-        #                         nn.MaxPool3d(2))
+#       self.conv4 = nn.Sequential(
+#                    nn.Conv3d(4*inter_num_ch, 2*inter_num_ch, kernel_size=3, padding=1),
+#                    nn.ReLU(inplace=True),
+#                    nn.BatchNorm3d(2*inter_num_ch),
+#                    nn.MaxPool3d(2))
         
         embed_dim = 1*5*32*32
         self.self_attn_3D = SelfAttention3D(embed_dim = embed_dim, in_channels = 1, dropout=dropout) # (1,5,32,32) --> (1,5,32,32)
@@ -71,14 +70,13 @@ class selfattn_3DCNN(nn.Module):
         # Add code to unsqueeze because we only have 1 channel (axis=1) of this 3d image (comes in (N, H, W), need to make into (N, C, H, W))
         trans_x = trans_x.unsqueeze(axis=1)
         
-        out = self.conv1(trans_x)
-        out = self.conv2(out)
-        out = self.conv3(out)
-        #         print(conv_out.shape)
-        #         conv4 = self.conv4(conv3)
-        #         print(conv4.shape)
+        conv_out = self.conv1(trans_x)
+        conv_out = self.conv2(conv_out)
+        conv_out = self.conv3(conv_out)
+        # conv4 = self.conv4(conv3)
+        # print(conv4.shape)
         out = self.self_attn_3D(out)
-        out = flatten(out)
+        out = flatten(conv_out)
         out = self.lin1(out)
         out = self.lin2(out)
         out = self.lin3(out)
